92ad2cee4a804fee86a3158d25359aa156c89c05
Reinual <michel.launier@videotron.ca>
2011-05-24 13:38:15 -0400

    Initial Commit

---
+++ BingGaBong/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BingGaBong
+//
+//  Created by Michel Launier on 11-05-24.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ BingGaBong/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BingGaBong
+//
+//  Created by Michel Launier on 11-05-24.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
